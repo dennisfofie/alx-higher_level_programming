e8b4d457ea62ef7de1256f1b04a9b47eee745d7c
dennisfofie <dennisbeckham000@gmail.com>
2022-09-25 19:34:42 +0000

    importing add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,17 +3,8 @@
 
 from add_0 import add
 
-"""
-args:
-    a = first arg
-    b = second arg
-
-Return:
-    addition of a + b
-"""
 a = 1
 b = 2
 
 # calling function add from 0_add.py to sum a and b
-print("{} + {} = {}".format(a, b, add(a,b)))
-
+print("{} + {} = {}".format(a, b, add(a, b)))
