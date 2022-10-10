75f26a9fdd8696bb797aebf220c2563eed67e1f2
dennisfofie <dennisbeckham000@gmail.com>
2022-10-10 15:37:16 +0000

    element of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,4 +10,6 @@ def safe_print_list(my_list=[], x=0):
                 count += 1
             except IndexError:
                 print()
+                return (count)
+    print()
     return (count)
