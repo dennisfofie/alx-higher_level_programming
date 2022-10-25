4ee95d48aa71ac19c91d73248c2606631b2c017b
dennisfofie <dennisbeckham000@gmail.com>
2022-10-25 20:47:48 +0000

    reading from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@ read_file - read file utf8 to stdout
 """
 def read_file(filename=""):
     with open("filename", mode="r", encoding="utf-8") as f:
-        result = f.read()
-        print(result)
+        for line in f:
+            print(line, end="")
