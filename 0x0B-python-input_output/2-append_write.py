6530feb503719ea74f6b715d1c18b4fb97a6f501
dennisfofie <dennisbeckham000@gmail.com>
2022-10-25 21:26:00 +0000

    appending to file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,5 +4,6 @@ append to file
 """
 
 def append_write(filename="", text=""):
+    """appends to file or create"""
     with open(filename, mode='a+', encoding='utf-8') as f:
         return (f.write(text))
