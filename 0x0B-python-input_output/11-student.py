f9be6fdbb49d77ca3ebe4c869d27bb53da016f6a
dennisfofie <dennisbeckham000@gmail.com>
2022-10-25 21:52:43 +0000

    done

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -10,6 +10,15 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """prints __dict__"""
-        return self.__dict__
+        if(attrs and isinstance(attrs, list) and
+           all(isinstance(x, str) for x in attrs)):
+            return({x: y for x, y in self.__dict__.items() if x in attrs})
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces allattributes with json"""
+        if(json):
+            self.__dict__ = json
