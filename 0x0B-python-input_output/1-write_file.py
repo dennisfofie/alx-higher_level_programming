8988806c52a4682137b149b36c34fa26f78af8a9
dennisfofie <dennisbeckham000@gmail.com>
2022-10-25 21:16:05 +0000

    writing to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-write_file - write to a file
-"""
+"""writes to a file"""
+
 
 def write_file(filename="", text=""):
-
-    with open(filename, mode="w", encoding="utf-8") as f:
-        return (f.write(text))
+    """writes to file"""
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return(f.write(text))
