6eaee044daf41d1064d1c5ede37e94ac72df2a6f
dennisfofie <dennisbeckham000@gmail.com>
2023-01-19 07:37:10 +0000

    making it executable

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,30 +2,16 @@
 """ Finds a peak in a list of unsorted integers
 """
 
+def search(lo, h, ints):
+    mid = (lo + h) // 2
+    if lo == h:
+        return ints[h]
+    if ints[mid] < ints[mid + 1]:
+        return(search(mid + 1, h, ints))
+    return(search(lo, mid, ints))
+
 
 def find_peak(list_of_integers):
-    """
-    Args:
-        list_of_integers(int): list of integers to find peak of
-    Returns: peak of list_of_integers or None
-    """
-    size = len(list_of_integers)
-    mid_e = size
-    mid = size // 2
-
-    if size == 0:
-        return None
-
-    while True:
-        mid_e = mid_e // 2
-        if (mid < size - 1 and
-                list_of_integers[mid] < list_of_integers[mid + 1]):
-            if mid_e // 2 == 0:
-                mid_e = 2
-            mid = mid + mid_e // 2
-        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-            if mid_e // 2 == 0:
-                mid_e = 2
-            mid = mid - mid_e // 2
-        else:
-            return list_of_integers[mid]
+    if not list_of_integers:
+        return
+    return(search(0, len(list_of_integers) - 1, list_of_integers))
